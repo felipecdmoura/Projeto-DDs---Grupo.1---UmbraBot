7029d113c3d47423e95095648016a05e5c68fd05
Felipe Candido de Moura <62444107+felipecdmoura@users.noreply.github.com>
2021-03-29 19:40:38 -0300

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
+const { MessageMentions, ClientUser, TextChannel } = require("discord.js");
 const Discord = require("Discord.js")
 const client = new Discord.Client()
 const config = require("./config.json")
+
 
 /*
 Nota:
@@ -10,6 +12,7 @@ client.on é um evento
 client.on("ready", () => {
     console.log(`O bot foi iniciado, com ${client.users.cache.size} usuários e em ${client.guilds.cache.size} servidores.`);
     client.user.setActivity(`Eu estou em ${client.guilds.cache.size} servidores`,{ type: 'PLAYING' });
+    console.log(client.guilds.cache.filter(g => g.memberCount < 10).map(g => g.name).join("\n"));
     // ^^ Texto que aparece no "jogando" do bot ^^
 
    /*
@@ -33,33 +36,99 @@ client.on("guildDelete", guild => {
     client.user.setActivity(`Serving ${client.guilds.cache.size} servers`)
 })
 
+
+
+// Função para o bot reconhecer comandos de mensagens
 client.on("message",async message =>{
-  if(message.author.bot) 
-  return;
+  if(message.author.bot) return;
     // Não responder outras coisas
-  if(message.channel.type === "dm") 
-  return;
+  if(message.channel.type === "dm") return;
     // Não responder chat privado
   if(!message.content.startsWith(config.prefix)) return;
 
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const comando = args.shift().toLowerCase();
   
-  // Função
-  if(comando === "ola") {
-    const m = await message.channel.send("...");
-    m.edit("Olá! Eu sou o bot de teste.");
+  // Comandos 
+  
+  if(comando === "kick") {
+    const usuario = message.mentions.users.first();
+    
+    if(!message.member.hasPermission("KICK_MEMBERS")) return message.reply("Você não tem permisssão!");
+
+    if(usuario){
+      const membro = message.guild.member(usuario);
+
+      if(membro){
+        membro.kick("razao").then(() =>{
+            message.reply(`Usuário ${usuario.tag} kickado com sucesso!`)
+        })
+        .catch(err => {
+          message.reply("Falha ao kickar o usuário");
+          console.log(err);
+        })
+      }else{
+        message.reply("O usuário não é membro do servidor!")
+      }
+    }else{
+      message.reply("Você não mencionou um usuário!");
+    }
+  }  
+
+  if(comando === "del") {
+
+    if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("Você não tem permisssão!");
+
+    if(!args[0]) return message.reply("Insira a quantidade de mensagens a serem deletadas!");
+    if(isNaN(args[0])) return message.reply("Insira um numero real!");
+    if(args[0] > 100) return message.reply("O máximo de mensagens deletadas é 100");
+    if(args[0] < 1) return message.reply("Pelo menos uma mensagem deve ser deletada!");
+      
+    message.channel.bulkDelete(args[0]);
   }
-  if(comando === "objetivo") {
-    const m = await message.channel.send("...");
-    m.edit("Meu objetivo é servir de teste para o melhor funcionamento do bot principal do projeto de DS.");
+
+  if(comando === "mute"){
+    const usuario = message.mentions.users.first();
+
+    if(!message.member.hasPermission("MUTE_MEMBERS")) return message.reply("Você não tem permisssão!");
+    if(usuario){
+      const membro = message.guild.member(usuario);
+
+      if(membro){
+        membro.voice.setMute(true)
+        .catch(err => {
+          message.reply("Falha ao mutar o usuário");
+          console.log(err);
+        })
+      }else{
+        message.reply("O usuário não é membro do servidor!")
+      }
+    }else{
+      message.reply("Você não mencionou um usuário!");
+    }
   }
-  if(comando === "data") {
-    const m = await message.channel.send("...");
-    var data = new Date()
-    m.edit("Meu objetivo é servir de teste para o melhor funcionamento do bot principal do projeto de DS.");
+
+  if(comando === "unmute"){
+    const usuario = message.mentions.users.first();
+
+    if(!message.member.hasPermission("MUTE_MEMBERS")) return message.reply("Você não tem permisssão!");
+    if(usuario){
+      const membro = message.guild.member(usuario);
+
+      if(membro){
+        membro.voice.setMute(false)
+        .catch(err => {
+          message.reply("Falha ao desmutar o usuário");
+          console.log(err);
+        })
+      }else{
+        message.reply("O usuário não é membro do servidor!")
+      }
+    }else{
+      message.reply("Você não mencionou um usuário!");
+    }
   }
 })
 
-client.login(config.token);
-// Seta o token
+// Token de acesso do bot
+client.login(config.BOT_TOKEN);
