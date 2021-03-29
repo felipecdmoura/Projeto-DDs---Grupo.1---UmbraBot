559e80d7b84034d323c6bd7ba1604abeb3263db6
Felipe Candido de Moura <62444107+felipecdmoura@users.noreply.github.com>
2021-03-29 20:15:49 -0300

    Delete ping.js

--- ping.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  name: `ping`,
-  description: `This is a basic ping-pong command (mostly for testing purposes).`,
-  execute(message, ARGS) {
-    message.channel.send(`pong!`);
-  },
-};
