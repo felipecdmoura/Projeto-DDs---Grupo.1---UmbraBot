464d430782f77e01bd9736fb822c6c343334c50a
Elios630 <78980830+Elios630@users.noreply.github.com>
2021-05-04 20:10:01 -0300

    ARGS
    ARGS maiúsculo

--- commands/stop.js
+++ commands/stop.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'stop',
     description: 'stop the music bot and leave the channel',
     syntax: '--stop',
-    async execute(message, args) {
+    async execute(message, ARGS) {
         const voiceChannel = message.member.voice.channel;
  
         if(!voiceChannel) return message.channel.send("You need to be in a voice channel to stop the music!");
